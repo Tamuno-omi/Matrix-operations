0f7e0d20c3f3b42904fd95c18a5885c2ed275e3b
Tamuno-omi <tamjaja@yahoo.com>
2021-01-21 18:33:59 +0000

    Implemented subMatrix method for subtraction of matrices

--- matrix_ops_dsnd/matrix_operations.py
+++ matrix_ops_dsnd/matrix_operations.py
@@ -57,3 +57,24 @@ class Matrix:
                 print('\n')
         else:
             print(x)
+            
+    def subMatrix(self,x,y):
+        """ Matrix object for subtraction of two matrices
+    
+        Arguments
+        x : first matrix
+        y : second matrix
+    
+        Returns
+        sum: result of the subtraction of two matrices
+        """
+        if(len(x) == len(y) and len(x[0]) == len(y[0])):
+            sum = [[0 for j in range(0,len(x[0]))] for i in range(0,len(x))]
+            for i in range(0, len(x)):
+                for j in range(0, len(x[0])):
+                    sum[i][j] = x[i][j] - y[i][j]
+            return sum
+
+        else:
+            error = "Matrices of different order"
+            return error
